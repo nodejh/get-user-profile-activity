620909738bb0e45abe9c23a97f8626b44d12db44
nodejh <replyjh@gmail.com>
2015-09-07 15:22:33 +0800

    完成抓取动态操作

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # get-user-profile-activity
 
 抓取知乎上指定用户的最新动态，并自动发送邮件
+
++ config.js 配置文件
++ index.js 主文件
++ time.txt 存储最新动态的时间
---
+++ config.js
@@ -0,0 +1,17 @@
+var config = {
+  db: {
+    host: 'locahost',
+    user: 'root',
+    password: 'root',
+    database: 'zihhu',
+  },
+  transporter: {
+    service: 'Gmail'
+  },
+  zhihu: {
+    user_link: 'http://www.zhihu.com/people/nodejh'
+  }
+};
+
+
+module.exports = config;
